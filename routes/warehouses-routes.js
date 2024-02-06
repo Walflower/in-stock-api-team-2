9452ef6187c65f3a748c3fc2a47c03aa29730a1d
Nyajal Wal <nyajal.w@gmail.com>
2024-02-06 15:26:08 -0500

    added get request for warehouse list and single warehouse.

---
+++ routes/warehouses-routes.js
@@ -0,0 +1,12 @@
+const router = require("express").Router();
+const warehousesController = require("../controllers/warehouses-controller");
+
+router
+  .route("/")
+  .get(warehousesController.getWarehouses)
+  .post(warehousesController.add);
+
+router.route("/:id").get(warehousesController.findOne);
+
+// need get,post(add),patch(edit),delete
+module.exports = router;
