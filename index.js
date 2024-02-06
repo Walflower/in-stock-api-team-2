f63ba8609764e3caa7d272ffc9e86ae62f18288f
Nyajal Wal <nyajal.w@gmail.com>
2024-02-05 22:34:43 -0500

    develop is ready to pull from

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 const express = require("express");
 const app = express();
+require("dotenv").config();
+const cors = require("cors");
+const PORT = process.env.PORT || 8000;
+const { BASE_PATH } = process.env;
+
+app.use(express.json());
+app.use(cors());
+
+app.use("/", (req, res, next) => {
+  console.log(req.url);
+  console.log("time", Date.now());
+  next();
+});
+
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));
