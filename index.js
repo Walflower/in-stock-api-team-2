4c2f0bef6b653525ac0f4ff1dddf2ea1b79ae7b9
Nyajal Wal <nyajal.w@gmail.com>
2024-02-06 13:01:33 -0500

    first commit

--- index.js
+++ index.js
@@ -4,14 +4,21 @@ require("dotenv").config();
 const cors = require("cors");
 const PORT = process.env.PORT || 8000;
 const { BASE_PATH } = process.env;
+const warehouses = require("./routes/warehouses-routes");
+const inventories = require("./routes/inventories-routes");
 
+//middlware
 app.use(express.json());
 app.use(cors());
 
+//Here just so we can see when the client is hitting the api. Can remove later!
 app.use("/", (req, res, next) => {
   console.log(req.url);
   console.log("time", Date.now());
   next();
 });
+
+app.use("/warehouses", warehouses);
+app.use("/inventories", inventories);
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
