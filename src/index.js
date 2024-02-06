ade4e1b5e3376cef9d5e649951a273274a0e0524
Indyanna Ormsby <indyanna.developer@gmail.com>
2024-02-06 12:37:42 -0800

    made the index match the env parameters

--- src/index.js
+++ src/index.js
@@ -2,16 +2,16 @@ const express = require("express");
 const app = express();
 require("dotenv").config();
 const cors = require("cors");
-const PORT = process.env.PORT || 8000;
-const { BASE_PATH } = process.env;
+const PORT = process.env.DB_PORT || 8000;
+const { BASE_PATH } = process.env.BASE_PATH;
 
 app.use(express.json());
 app.use(cors());
 
 app.use("/", (req, res, next) => {
-  console.log(req.url);
-  console.log("time", Date.now());
-  next();
+	console.log(req.url);
+	console.log("time", Date.now());
+	next();
 });
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
