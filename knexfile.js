3c14ce4961761a42be9bdfbe8c598057f86d7456
Sima rajabi <sima.rajabi24@gmail.com>
2024-02-08 23:34:30 -0800

    modified the Api for getting inventory list

--- knexfile.js
+++ knexfile.js
@@ -4,16 +4,17 @@ module.exports = {
 	client: "mysql2",
 	connection: {
 		host: process.env.DB_HOST,
-		port: process.env.DB_PORT,
+		// port: process.env.DB_PORT,
 		database: process.env.DB_NAME,
 		user: process.env.DB_USER,
 		password: process.env.DB_PASSWORD,
-		path: process.env.DB_PATH,
+		charset : "utf8",
+		// path: process.env.DB_PATH,
 	},
-	migrations: {
-		directory: "./database/migrations",
-	},
-	seeds: {
-		directory: "./database/seeds",
-	},
+	// migrations: {
+	// 	directory: "./database/migrations",
+	// },
+	// seeds: {
+	// 	directory: "./database/seeds",
+	// },
 };
