f979de8496a8beb85777a2fda6e14e2ab0da0729
Sima rajabi <sima.rajabi24@gmail.com>
2024-02-10 12:28:00 -0800

    removed spaces

--- knexfile.js
+++ knexfile.js
@@ -4,11 +4,9 @@ module.exports = {
 	client: "mysql2",
 	connection: {
 		host: process.env.DB_HOST,
-		// port: process.env.PORT,
 		database: process.env.DB_NAME,
 		user: process.env.DB_USER,
 		password: process.env.DB_PASSWORD,
 		charset : "utf8",
-		// path: process.env.DB_PATH,
 	},
 };
